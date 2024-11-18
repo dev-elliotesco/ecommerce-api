63317d1050e57a44febee24547045e6e300a79ff
Elliot Escovitch Riaño <eescovit@bancolombia.com.co>
2024-11-18 11:07:24 -0500

    add find by term

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -6,6 +6,8 @@ import { Product } from './entities/product.entity';
 import { Repository } from 'typeorm';
 import { PaginationDto } from 'src/common/dtos/pagination.dto';
 
+import {validate as isUUID} from 'uuid';
+
 @Injectable()
 export class ProductsService {
 
@@ -36,11 +38,19 @@ export class ProductsService {
     });
   }
 
-  async findOne(id: string) {
-    const product = await this.productRepository.findOneBy({id});
+  async findOne(term: string) {
+
+    let product: Product;
+
+    if(isUUID(term)){
+      product = await this.productRepository.findOneBy({id: term});
+    }
+    else{
+      product = await this.productRepository.findOneBy({slug: term});
+    }
 
     if(!product){
-      throw new NotFoundException(`Product with id ${id} not found`);
+      throw new NotFoundException(`Product with id ${term} not found`);
     }
 
     return product;
